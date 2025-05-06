9b62682d6ca5ba44ad763b45568380017965f665
Cephaz6 <cephasblog@gmail.com>
2025-05-06 21:18:27 +0100

    Roadmap Added

--- main.py
+++ main.py
@@ -2,16 +2,25 @@ from fastapi import FastAPI
 from fastapi.params import Body  
 from pydantic import BaseModel
 from typing import Optional
+from random import randrange 
 
 
 
 app = FastAPI()
 
 class Post(BaseModel):
+    id: int = randrange(0, 1000000)
     title: str
     content: str
     published: bool = True
     rating: Optional[int] = None
+
+
+my_posts = [
+    {"id": 1, "title": "Post 1", "content": "Content of post 1", "published": True, "rating": 5},
+    {"id": 2, "title": "Post 2", "content": "Content of post 2", "published": False, "rating": 3},
+    {"id": 3, "title": "Post 3", "content": "Content of post 3", "published": True, "rating": 4},   
+]
 
 
 @app.get("/")
@@ -20,15 +29,11 @@ def read_root():
 
 @app.get("/posts")
 def get_posts():
-    return [
-        {"data": "This is a sample post 1"},
-        {"data": "This is a sample post 2"},
-        {"data": "This is a sample post 3"},
-    ]
+    return {"posts": my_posts}
 
 @app.post("/createposts")
 def create_posts(post: Post):
     print(post)
     print(post.model_dump())
-    
+    my_posts.append(post.model_dump())
     return {"data": f"Post created with title: {post.title} and content: {post.content}"}
