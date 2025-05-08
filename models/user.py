f796a4a65537f8816024c8d02db1ac4d802e3e68
Cephaz6 <cephasblog@gmail.com>
2025-05-08 01:15:21 +0100

    Setting Up User DB, Schema and Authentication Files

---
+++ models/user.py
@@ -0,0 +1,12 @@
+from sqlmodel import SQLModel, Field
+from typing import Optional
+from datetime import datetime
+
+class User(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    email: str = Field(index=True, unique=True)
+    hashed_password: str
+    full_name: Optional[str] = None
+    is_active: bool = Field(default=True)
+    created_at: datetime = Field(default_factory=datetime.now)
+    updated_at: datetime = Field(default_factory=datetime.now, sa_column_kwargs={"onupdate": datetime.now})
