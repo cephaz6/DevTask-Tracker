db756e3c9f348459e1d9209043b45cb0d4e0909d
Cephaz6 <cephasblog@gmail.com>
2025-05-21 23:55:19 +0100

    Update

--- utils/core.py
+++ utils/core.py
@@ -1,6 +1,8 @@
 import secrets, string
 
+# This file contains utility functions for the application.
+# The functions are used to generate random strings for user IDs and passwords.
 def generate_user_id():
-    
+
     alphabet = string.ascii_lowercase + string.digits
     return ''.join(secrets.choice(alphabet) for i in range(16))
